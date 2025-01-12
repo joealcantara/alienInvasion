047a3e4c342ba3635b69ae16dd6affeacd818564
Joe Alcantara <joe@joealcantara.com>
2025-01-12 12:56:42 +0000

    added a ship image and a ship class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """
@@ -19,24 +20,33 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasions")
 
-        # Set Background Colour as an attribute of the game.
-        # self.bg_colour = (230, 230, 230)
-    
+        self.ship = Ship(self)
+
     def run_game(self):
         """
         Start the main loop for the game.        
         """
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_colour)
-            pygame.display.flip()
+            self._check_events()    # Watch for keyboard and mouse events.
+            self._update_screen()   # Redraw the screen during each pass through the loop.
             self.clock.tick(60)
-    
+
+    def _check_events(self):
+        """
+        Respond to keypresses and mouse events.
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """
+        Updates images on the screen and flip to the new screen.
+        """
+        self.screen.fill(self.settings.bg_colour)
+        self.ship.blitme()
+        pygame.display.flip()
+
 if __name__ == '__main__':      
     # Make a game instance, and run the game.
     ai = AlienInvasion()
