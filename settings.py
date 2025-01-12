8e6954d6aa85957061094bc2ee7bad57cb2f3439
Joe Alcantara <joe@joealcantara.com>
2025-01-12 12:23:34 +0000

    added settings.py to store all settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """
+    A class to store all settings for Alien Invasions.
+    """
+    def __init__(self):
+        """
+        Initialise the game's settings.
+        """
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
